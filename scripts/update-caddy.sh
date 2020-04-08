57006ae4e6f1699426e2ddec37c596648f1b2b13
aitva <aitva@pm.me>
2020-04-08 18:41:32 +0200

    add script to update caddy

---
+++ scripts/update-caddy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+INSTALLDIR="/usr/local/caddy"
+
+VERSION=$1
+if [ -z $VERSION ]; then
+    >&2 echo "usage: $0 VERSION"
+    exit 1
+fi
+
+if [ "$EUID" -ne 0 ]; then
+    echo "this script should be run as root"
+    exit 1
+fi
+
+URL="https://github.com/caddyserver/caddy/releases/download"
+URL+="/v${VERSION}/caddy_${VERSION}_Linux_x86_64.tar.gz"
+
+curl --location --silent $URL -o caddy.tgz
+if [ -d $INSTALLDIR ]; then
+    rm -rf $INSTALLDIR
+fi
+
+mkdir $INSTALLDIR
+tar -C $INSTALLDIR -xf caddy.tgz
+rm caddy.tgz
+
+$INSTALLDIR/caddy version
