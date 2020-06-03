b7368f681dc5847f22798de09e6f51686635f6ac
aitva <aitva@pm.me>
2020-06-03 10:52:00 +0200

    fix update-go-tools fail without GO111MODULE=on

--- scripts/update-go-tools.sh
+++ scripts/update-go-tools.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export GO111MODULE=on
 go get golang.org/x/tools/gopls@latest
 go get golang.org/x/tools/cmd/goimports@latest
 go get github.com/golangci/golangci-lint/cmd/golangci-lint@v1.22.2
