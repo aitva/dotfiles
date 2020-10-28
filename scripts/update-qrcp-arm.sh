b09f30d39d62d7a4fb7c3721c8e4138c3aedbf6b
aitva <arod.louis@gmail.com>
2020-10-28 10:15:04 +0100

    A  scripts/update-qrcp-arm.sh A  scripts/update-qrcp.sh

---
+++ scripts/update-qrcp-arm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+INSTALLDIR="$HOME/.bin"
+BINARYPATH="$INSTALLDIR/qrcp"
+
+VERSION=$1
+if [ -z $VERSION ]; then
+    >&2 echo "usage: $0 VERSION"
+    exit 1
+fi
+
+URL="https://github.com/claudiodangelis/qrcp/releases/download"
+URL+="/${VERSION}/qrcp_${VERSION}_linux_arm64.tar.gz"
+
+curl --location --silent $URL -o qrcp.tgz
+if [ -f $BINARYPATH ]; then
+    rm $BINARYPATH
+fi
+tar -C $INSTALLDIR -xf qrcp.tgz qrcp
+
+rm qrcp.tgz
+$BINARYPATH version
