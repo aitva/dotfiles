555443dfd7eab710ab2a8f2ea46eed43fe51d1ed
aitva <aitva@pm.me>
2022-05-18 10:19:10 -0700

    update go tools

--- scripts/update-go-tools.sh
+++ scripts/update-go-tools.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 go install golang.org/x/tools/gopls@latest
 go install golang.org/x/tools/cmd/goimports@latest
-go install github.com/kyleconroy/sqlc/cmd/sqlc@v1.10.0
+go install github.com/kyleconroy/sqlc/cmd/sqlc@v1.13.0
 go install github.com/valyala/quicktemplate/qtc@latest
 
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh \
-	| sh -s -- -b $(go env GOPATH)/bin v1.45.0
+	| sh -s -- -b $(go env GOPATH)/bin v1.45.2
 golangci-lint --version
